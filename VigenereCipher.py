825c847098937360b4997aebf41f82ac5cc73382
JoelHMikael <53561102+JoelHMikael@users.noreply.github.com>
2020-03-07 17:35:51 +0200

    Vigenere Cipher created
    (copypasted from my hard drive)

---
+++ VigenereCipher.py
@@ -0,0 +1,49 @@
+from getpass import getpass
+
+def crypt(mode, s, key, abc = "abcdefghijklmnopqrstuvwxyz"):
+    upper = []
+    
+    s = list(s)
+    key = list(key)
+    
+    i = 0
+    while i < len(s):
+        if s[i].isupper():
+            upper.append(i)
+            s[i] = s[i].lower()
+        if (s[i] in abc) & (key[i % len(key)] in abc):
+            a = abc.index(s[i])
+            b = abc.index(key[i % len(key)])
+            if mode.lower() == "encrypt":
+                s[i] = abc[(a + b) % len(abc)]
+            else:
+                s[i] = abc[(a - b) % len(abc)]
+            #s[i] = abc[abc.index(s[i]) + abc.index(key[i % len(key)])]
+        i += 1
+    
+    for i in upper:
+        s[i] = s[i].upper()
+    
+    temp = ""
+    return temp.join(s)
+
+def cryptfile(mode, file, newfile, key, cryptfunction, abc = "abcdefghijklmnopqrstuvwxyz"):
+    file = open(file, "r")
+    fcont = file.read()
+    file.close()
+    newfile = open(newfile, "w")
+    try:
+        newfile.write(cryptfunction(mode, fcont, key, abc))
+    except Exception as e:
+        print(str(e) + " when trying to write to the new file!")
+    newfile.close()
+
+
+c = "y"
+while c == "y":
+    file = input("File? ")
+    newf = input("File to write the result to? ")
+    mode = input("Encrypt/decrypt? ")
+    key = getpass("Key? ")
+    cryptfile(mode, file, newf, key, crypt)
+    c = input("Next file? (y/n)")
